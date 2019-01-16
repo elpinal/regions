d086d404fab2c78cda17f54d52982958fc1e7c7e
elpinal <elpinal@users.noreply.github.com>
2019-01-16 17:57:27 +0900

    Init with Leiningen

---
+++ clj/project.clj
@@ -0,0 +1,7 @@
+(defproject regions "0.1.0-SNAPSHOT"
+  :description "Generate a bibliography"
+  :url ""
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main regions.core)
